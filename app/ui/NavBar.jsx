c6272718f210bc2a0b103920b21551847b225f18
blanca-almandoz <blanca.almandoz@coverwallet.com>
2024-03-21 17:56:36 +0100

    fix: handleClick navbar

--- app/ui/NavBar.jsx
+++ app/ui/NavBar.jsx
@@ -1,5 +1,5 @@
 "use client";
-
+import { useState } from 'react'
 import Image from "next/image"
 import Link from "next/link"
 import { usePathname } from 'next/navigation';
@@ -7,18 +7,19 @@ import clsx from 'clsx';
 
 
 const NavBar = () => {
+  const [onOpen, setOnOpen] = useState(false)
   const pathname = usePathname();
+
+  const handleClick = () => {
+    setOnOpen(!onOpen)
+  }
 
     return (<nav className="bg-gray-800">
     <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
       <div className="relative flex h-16 items-center justify-between">
         <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
          {/*  <!-- Mobile menu button--> */}
-          <button type="button" className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
-            <span className="absolute -inset-0.5"></span>
-            <span className="sr-only">Open main menu</span>
-            {/* <!-- Icon when menu is closed.
-              Menu open: "hidden", Menu closed: "block"--> */}
+          <button onClick={handleClick} type="button" className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
             <svg className="block h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
               <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
             </svg>
@@ -57,22 +58,22 @@ const NavBar = () => {
     </div>
   
    {/*  <!-- Mobile menu, show/hide based on menu state. --> */}
-    <div className="sm:hidden" id="mobile-menu">
+    {onOpen && <div className="sm:hidden" id="mobile-menu">
       <div className="space-y-1 px-2 pb-3 pt-2">
-        <Link href="/recipes/starters" className={clsx("text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium",
+        <Link href="/recipes/starters" onClick={handleClick} className={clsx("text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium",
                                               {'bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium' : pathname === '/recipes/starters'},
-                                        )}>Tapas y aperitivos</Link>
-        <Link href="/recipes/main" className={clsx("text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium",
+                                        )}>Tapas y aperitivos blanca</Link>
+        <Link href="/recipes/main" onClick={handleClick} className={clsx("text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium",
                                                             {'bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium' : pathname === '/recipes/main'},
                                                       )}>Principales</Link>
-        <Link href="/recipes/desserts" className={clsx("text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium",
+        <Link href="/recipes/desserts" onClick={handleClick} className={clsx("text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium",
                                                             {'bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium' : pathname === '/recipes/desserts'},
                                                       )}>Postres</Link>
-        <Link href="/recipes/siders" className={clsx("text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium",
+        <Link href="/recipes/siders" onClick={handleClick} className={clsx("text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium",
                                                             {'bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium' : pathname === '/recipes/siders'},
                                                       )}>Salsas y guarniciones</Link>
       </div>
-    </div>
+    </div>}
   </nav>
   )
 } 
