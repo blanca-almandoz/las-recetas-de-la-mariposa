a96a9553906b4b781222a9c7ca11c41b6a2c9300
Blanca Almandoz <blanca.almandoz@gmail.com>
2024-04-02 11:19:00 +0200

    feat: sort recipes alfabetically

--- app/lib/utils.js
+++ app/lib/utils.js
@@ -1,9 +1,17 @@
-
-
-export function fetchRecipesById (recipes, id){
-    return recipes.filter((recipe)=> recipe.id === id)
+export function fetchRecipesById(recipes, id) {
+  return recipes.filter((recipe) => recipe.id === id);
 }
 
-export function fetchRecipesByCategory (recipes, category) {
-    return recipes.filter((recipe)=> recipe.category === category)
+export function fetchRecipesByCategory(recipes, category) {
+  const json = recipes.filter((recipe) => recipe.category === category);
+  const res = json.sort((a, b) => {
+    if (a.title < b.title) {
+      return -1;
+    }
+    if (a.title > b.title) {
+      return 1;
+    }
+    return 0;
+  });
+  return res;
 }
