a9b80730b67f40198fe11ed91e1bd11d035e248e
Blanca Almandoz <blanca.almandoz@straumann.com>
2024-08-15 21:01:30 +0200

    feat: fix styles

--- app/ui/search/Search.tsx
+++ app/ui/search/Search.tsx
@@ -24,20 +24,17 @@ const Search = () => {
 
   return (
     <div className={searchWrapper}>
-      <div className={searchComponent}>
-        <input
-          type="text"
-          name="search"
-          id="search"
-          className={searchInput}
-          placeholder="Buscar..."
-          onChange={(e) => {
-            handleSearch(e.target.value)
-          }}
-          defaultValue={searchParams.get('query')?.toString()}
-        />
-        <div className={searchCloseButton}>x</div>
-      </div>
+      <input
+        type="text"
+        name="search"
+        id="search"
+        className={searchInput}
+        placeholder="Buscar..."
+        onChange={(e) => {
+          handleSearch(e.target.value)
+        }}
+        defaultValue={searchParams.get('query')?.toString()}
+      />
     </div>
   )
 }
